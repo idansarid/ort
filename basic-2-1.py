a99cef18bc1961ea7ba84b98f4701aaafced93b4
idan sarid <idan.sarid@wdc.com>
2020-12-10 10:32:53 +0200

    new files ort
    Signed-off-by: idan sarid <idan.sarid@wdc.com>

---
+++ basic-2-1.py
@@ -0,0 +1,38 @@
+var = "hello world!"
+var1 = 5
+var2 = 5.0
+var3 = True
+print(type(var))
+print(type(var1))
+print(type(var2))
+print(type(var3))
+
+reverse_var = var[::-1]
+splitted = var.split(" ")
+var = "agam"
+if var.islower() == True:
+    print("string with lower chars")
+elif var.isupper() == True:
+    print("string with upper chars")
+else:
+    pass
+
+lst = [1, 2, 3, 4, 5]
+# print(lst.count(1))
+
+for i in lst:
+    print(i)
+
+for i in range(100):
+    print(i)
+
+i = 0
+while i < 10:
+    print("idan")
+    i += 1
+
+type(lst)
+
+user_input = input("please enter an input\n")
+
+y = "yotam"
---
+++ str-2-1.py
@@ -0,0 +1,13 @@
+str1 = 'w3resource.com'
+str3 = 'wwrong'
+print(str3.count('w'))
+str1[::]
+
+
+str1.count('w')
+str2 = str1.replace("3", "8")
+str1.find()
+str1.isdigit()
+str1.isdecimal()
+str1.split(" ")
+print(str1.find(sub="com"))
